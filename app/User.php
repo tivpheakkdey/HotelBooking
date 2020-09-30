def0ef1b6534977cb0bb38972ca446445a87e0e4
vtiv <vtiv@deakin.edu.au>
2020-09-30 21:36:56 +1000

    Implemented Profile model, migration, and Profile policy

--- app/User.php
+++ app/User.php
@@ -38,8 +38,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function($user){
+            $user->profile()->create();
+        });
+    }
+
     public function bookings()
     {
         return $this->hasMany(Booking::class)->orderBy('created_at','DESC');
+    }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
                                     <a class="dropdown-item" href="/user/{{Auth::user()->id}}">
-                                        Booking
+                                        Profile
                                     </a>
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
