8b70caf90e03b688cf606bd4b4603560db6f4428
Martin Tai <martintai@Martins-MacBook-Pro.local>
2020-10-01 19:26:14 +1000

    some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,23 @@
+<!DOCTYPE html>
 @extends('layouts.app')
 
+<body id="body">
 @section('content')
+<br/>
+<br/> 
+<br/>
+<br/> 
 <div class="container">
+	<div class="banner_content text-center">
+	    <h6>Welcome to</h6>
+		<h2>Paradise with a view</h2>
+		<p>If you are looking at blank cassettes on the web, you may be very confused at the<br> difference in price. You may see some for as low as $.17 each.</p>
+		<a href="#accomodation" class="btn theme_btn button_hover">Get Started</a>
+	</div>
+</div>
+
+
+<!--<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +35,7 @@
             </div>
         </div>
     </div>
-</div>
+</div>-->
 @endsection
+</body>
+</html>
