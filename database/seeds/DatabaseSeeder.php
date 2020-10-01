7bf7c77ea15013ac2baaec4eb0a00d67fbc5c694
vtiv <vtiv@deakin.edu.au>
2020-10-02 03:29:44 +1000

    resolve conflict

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,13 +13,5 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoomsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> fda8c1e... Created seeder for room
-=======
->>>>>>> f42dab3... implement the room controller, route, and view
-=======
->>>>>>> 44d92500d082e5a91f8b968ac1dce1ce380fda83
     }
 }
