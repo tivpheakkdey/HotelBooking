20086a99264b87c32141e7ffb144a380bd12e439
vtiv <vtiv@deakin.edu.au>
2020-10-02 04:41:55 +1000

    implemented working form for booking, need to store the booking data and check for availability

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Room;
 use Illuminate\Http\Request;
 
 class BookingsController extends Controller
@@ -14,10 +15,13 @@ class BookingsController extends Controller
             'arrival_date' => ['required','date','after_or_equal:today'],
             'departure_date' => ['required','date','after:arrival_date'],
             'num_room' => ['required', 'numeric'],
-            'amount' => ['required','numeric'],
+            'amount' => ['numeric'],
         ]);
 
-        dd($data);
+        $price = Room::find(request('room_id'))->price;
+        $total_price = $price * request('num_room');
+
+        dd(array_merge($data,['amount'=>$total_price]));
         // auth()->user()->bookings()->create($data);
     }
 }
--- resources/views/room/show.blade.php
+++ resources/views/room/show.blade.php
@@ -12,6 +12,46 @@
                     <div>description:{{$room->description}}</div>
                     <div>price{{$room->price}}</div>
                 </div>
+                @auth
+                    <div>
+                        <form method="POST" action="/booking">
+                            @csrf
+                            <input type="hidden" id="room_id" name="room_id" value="{{$room->id}}"><br><br>
+
+                            <div>
+                                <label for="arrival_date">Arrival Date:</label><br>
+                                <input type="date" id="arrival_date" name="arrival_date"><br><br>
+
+                                @error('arrival_date')
+                                        <strong>{{ $message }}</strong>
+                                @enderror
+                            </div>
+
+                            <div>
+                                <label for="departure_date">Departure Date:</label><br>
+                                <input type="date" id="departure_date" name="departure_date"><br><br>
+
+                                @error('departure_date')
+                                        <strong>{{ $message }}</strong>
+                                @enderror
+                            </div>
+
+                            <div>
+                                <label for="num_room">Number of room:</label><br>
+                                <input type="number" id="num_room" name="num_room" min="0" max="10"><br><br>
+                                @error('num_room')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <input type="submit" value="Submit">
+                        </form>
+                    </div>
+                @endauth
+
+
             </div>
         </div>
     </div>
