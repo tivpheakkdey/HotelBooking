b28019b01d4e4c2add0b4498764ee0f3b2dab959
vtiv <vtiv@deakin.edu.au>
2020-10-02 14:16:44 +1000

    added booking seed

---
+++ database/seeds/BookingsTableSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BookingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Booking::create([
+            'user_id'=> 1,
+            'room_id'=> 1,
+            'arrival_date' => '2020-10-03',
+            'departure_date' => '2020-10-05',
+            'num_room' => '2',
+            'amount' => '1000',
+        ]);
+
+        Booking::create([
+            'user_id'=> 1,
+            'room_id'=> 2,
+            'arrival_date' => '2020-10-04',
+            'departure_date' => '2020-10-10',
+            'num_room' => '3',
+            'amount' => '3600',
+        ]);
+
+        Booking::create([
+            'user_id'=> 1,
+            'room_id'=> 3,
+            'arrival_date' => '2020-10-03',
+            'departure_date' => '2020-10-05',
+            'num_room' => '1',
+            'amount' => '1500',
+        ]);
+
+        Booking::create([
+            'user_id'=> 2,
+            'room_id'=> 4,
+            'arrival_date' => '2020-10-04',
+            'departure_date' => '2020-10-10',
+            'num_room' => '4',
+            'amount' => '4800',
+        ]);
+
+        Booking::create([
+            'user_id'=> 2,
+            'room_id'=> 3,
+            'arrival_date' => '2020-10-03',
+            'departure_date' => '2020-10-05',
+            'num_room' => '1',
+            'amount' => '1500',
+        ]);
+    }
+}
